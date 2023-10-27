7eaaf115299331d8890e23ef4046c5b5cc83497f
Braian Ledantes <braianledantes@gmail.com>
2023-10-27 19:28:36 -0300

    link to github repo

--- TPO-Final/ElBardelaFAI/app/src/main/java/com/braianledantes/elbardelafai/ui/about/AboutActivity.kt
+++ TPO-Final/ElBardelaFAI/app/src/main/java/com/braianledantes/elbardelafai/ui/about/AboutActivity.kt
@@ -1,5 +1,7 @@
 package com.braianledantes.elbardelafai.ui.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
@@ -8,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.braianledantes.elbardelafai.databinding.ActivityAboutBinding
 
 class AboutActivity: AppCompatActivity() {
-    
+
     private lateinit var binding: ActivityAboutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +19,19 @@ class AboutActivity: AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.tvLinkToRepo.setOnClickListener {
+            val url = "https://github.com/braianledantes/LAB-PROG"
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(url)
+            )
+            startActivity(intent)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home){
+        if (item.itemId == android.R.id.home) {
             onBackPressedDispatcher.onBackPressed()
         }
         return super.onOptionsItemSelected(item)
