a57b9ec8b0afbce50eddfda27fcc4b1a9f2ed9cd
Braian Ledantes <braianledantes@gmail.com>
2023-10-27 19:16:19 -0300

    back button added

--- TPO-Final/ElBardelaFAI/app/src/main/java/com/braianledantes/elbardelafai/ui/about/AboutActivity.kt
+++ TPO-Final/ElBardelaFAI/app/src/main/java/com/braianledantes/elbardelafai/ui/about/AboutActivity.kt
@@ -1,6 +1,9 @@
 package com.braianledantes.elbardelafai.ui.about
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.braianledantes.elbardelafai.databinding.ActivityAboutBinding
 
@@ -12,5 +15,15 @@ class AboutActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home){
+            onBackPressedDispatcher.onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
